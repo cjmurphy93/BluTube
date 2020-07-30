f704dc0d4f05b093bd2d83e1eafa23a2f97fb392
cjmurphy93 <cjmurph93@gmail.com>
2020-07-29 22:19:25 -0400

    cleaning up schema

--- db/migrate/20200729175403_fix_users_table.rb
+++
@@ -1,7 +0,0 @@
-class FixUsersTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :username
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name, :string, null: false
-  end
-end
