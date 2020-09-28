86284f36a0fdd42f4dea1a7d3efe4b35564d8475
cjmurphy93 <cjmurph93@gmail.com>
2020-09-28 02:14:48 -0400

    comment util

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,36 @@
+export const fetchComments = (videoId) => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/videos/${videoId}/comments`
+    })
+);
+
+export const fetchComment = (commentId) => (
+  $.ajax({
+    method: "GET",
+    url: `/api/comments/${commentId}`,
+  })
+);
+
+export const createComment = (comment) => (
+    $.ajax({
+        method: 'POST',
+        url: `/api/videos/${comment.commentable_id}/comments`,
+        data: {comment}
+    })
+);
+
+export const updateComment = (comment) => (
+    $.ajax({
+        method: 'PATCH',
+        url: `/api/comments/${comment.id}`,
+        data: {comment}
+    })
+);
+
+export const deleteComment = (commentId) => (
+    $.ajax({
+        method: 'DELETE',
+        url: `/api/comments/${commentId}`
+    })
+);
