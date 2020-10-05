c5f194912789cd6c693b5864f4091da7cb3ce3c4
cjmurphy93 <cjmurph93@gmail.com>
2020-10-05 01:32:56 -0400

    cleaning up sidebar

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faAngellist } from '@fortawesome/free-brands-svg-icons';
 
 class Sidebar extends React.Component {
     constructor(props) {
@@ -11,17 +12,27 @@ class Sidebar extends React.Component {
     render() {
         let closed = !this.props.open ? "closed" : "";
         const sidebar = this.props.render ? (
-            <div className={`sidebar ${closed}`}>
-                <div className={`sidebar-top ${closed}`}>
-                    <Link to="/" className={`sidebar-home ${closed}`} >
-                        <FontAwesomeIcon icon={faHome} className="home-icon" />
-                        <span className={`home-text ${closed}`}>Home</span>
-                    </Link>
-                </div>
+          <div className={`sidebar ${closed}`}>
+            <div className={`sidebar-top ${closed}`}>
+              <div className="sidebar-link">
+                <Link to="/" className={`sidebar-home ${closed}`}>
+                  <FontAwesomeIcon icon={faHome} className="home-icon" />
+                  <span className={`home-text ${closed}`}>Home</span>
+                </Link>
+              </div>
+              <div className={`sidebar-home ${closed}`}>
+                <FontAwesomeIcon icon={faLinkedin} className="home-icon" />
+                <span className={`home-text ${closed}`}>LinkedIn</span>
+              </div>
+              <div className={`sidebar-home ${closed}`}>
+                <FontAwesomeIcon icon={faAngellist} className="home-icon" />
+                <span className={`home-text ${closed}`}>AngelList</span>
+              </div>
             </div>
+          </div>
         ) : (
-            <></>
-        )
+          <></>
+        );
         return (
             <div>
                 {sidebar}
