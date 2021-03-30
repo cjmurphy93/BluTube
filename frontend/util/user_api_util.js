e33a3b1536f63a4dd4f2210abbc03708462d5ba8
cjmurphy93 <cjmurph93@gmail.com>
2021-03-29 23:26:10 -0400

    added updateUser ajax action

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -9,3 +9,12 @@ export const fetchUser = (id) =>
     method: "GET",
     url: `/api/users/${id}`,
   });
+
+export const updateUser = (user, id) =>
+  $.ajax({
+    method: "PATCH",
+    url: `/api/users/${id}`,
+    data: user,
+    contentType: false,
+    processData: false,
+  });
