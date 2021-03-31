08523e4e94f192783203d7f5403872b2d9cddda1
cjmurphy93 <cjmurph93@gmail.com>
2021-03-31 14:13:41 -0400

    saved user edit component

--- frontend/components/profile/profile_edit.jsx
+++ frontend/components/profile/profile_edit.jsx
@@ -6,10 +6,28 @@ class ProfileEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.props.currentUser;
+    this.findFileInput = this.findFileInput.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUndo = this.handleUndo.bind(this);
+    this.handleProfilePic = this.handleProfilePic.bind(this);
   }
 
   componentDidMount() {
-    this.props.fetchUser(this.props.currentuser.id);
+    this.props.fetchUser(this.props.currentUser.id);
+  }
+
+  handleUndo(e) {
+    e.preventDefault();
+    if (
+      this.state !== this.props.currentUser &&
+      this.state.id === this.props.currentUser.id
+    ) {
+      this.setState(this.props.currentUser);
+      if (!this.props.currentUser.profilePicUrl) {
+        this.setState({ profilePicFile: undefined, profilePicUrl: undefined });
+      }
+    }
   }
 
   handleInput(field) {
@@ -46,6 +64,84 @@ class ProfileEdit extends React.Component {
     this.props.updateUser(user, this.props.currentUser.id).then(() => {
       return this.props.history.push(`/`);
     });
+  }
+
+  render() {
+    if (!this.props.currentUser || this.props.currentUser.id !== this.state.id)
+      return null;
+
+    const { profilePicFile, profilePicUrl } = this.state;
+
+    const publishButton =
+      this.props.currentUser.id === this.state.id &&
+      profilePicUrl !== this.props.profilePicUrl ? (
+        <button className="publish save-btn" onClick={this.handleSubmit}>
+          SAVE
+        </button>
+      ) : (
+        <></>
+      );
+
+    const profilePic = profilePicUrl ? (
+      <img
+        src={profilePicUrl}
+        alt="thumbnail"
+        className="thumbnail-preview"
+        onClick={this.findFileInput}
+      />
+    ) : (
+      <div className="thumbnail-upload-button" onClick={this.findFileInput}>
+        <FontAwesomeIcon className="thumbnail-icon" icon={faImage} />
+        <span>Upload profile picture</span>
+      </div>
+    );
+
+    const undoBtn =
+      this.props.currentUser.id === this.state.id &&
+      profilePicUrl !== this.props.profilePicUrl ? (
+        <button className="publish undo-btn" onClick={this.handleUndo}>
+          UNDO CHANGES
+        </button>
+      ) : (
+        <button className="publish undo-btn-disabled">UNDO CHANGES</button>
+      );
+
+    return (
+      <div className="edit-video">
+        <div className="edit-top">
+          <div className="edit-top-inner">
+            <div className="edit-top-bottom">
+              <h2>Profile details</h2>
+              <div className="edit-buttons">
+                {undoBtn}
+                {publishButton}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <section id="edc-2" className="upload-content-2 edc-2">
+          <section className="upload-details edd-2">
+            <div className="thumbnail-upload-form">
+              <h3>Profile Picture</h3>
+              <p>Select or upload a picture</p>
+              <div className="thumbnail-previews">
+                <div className="thumbnail-upload-container">
+                  {profilePic}
+                  <input
+                    type="file"
+                    name="file"
+                    id="file"
+                    accept=".jpg"
+                    onChange={this.handleProfilePic}
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+      </div>
+    );
   }
 }
 
