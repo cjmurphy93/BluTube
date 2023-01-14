3a1e6da4bf2f17139e28d25b16060be0905e1087
cjmurphy93 <cjmurph93@gmail.com>
2023-01-14 17:37:28 -0500

    updating

--- frontend/components/nav_bar/nav_bar_container.jsx
+++ frontend/components/nav_bar/nav_bar_container.jsx
@@ -1,43 +1,43 @@
-import { connect } from "react-redux";
-import { logout } from "../../actions/session_actions";
-import { openSidebar, closeSidebar } from "../../actions/sidebar_actions";
-import { openModal } from "../../actions/modal_actions";
-import { withRouter } from "react-router-dom";
-import NavBar from "./nav_bar";
+import { connect } from 'react-redux'
+import { logout } from '../../actions/session_actions'
+import { openSidebar, closeSidebar } from '../../actions/sidebar_actions'
+import { openModal } from '../../actions/modal_actions'
+import { withRouter } from 'react-router-dom'
+import NavBar from './nav_bar'
 
 const mapStateToProps = (
-  {
-    session,
-    entities: { users },
-    ui: {
-      modal,
-      sidebar: { open },
+    {
+        session,
+        entities: { users },
+        ui: {
+            modal,
+            sidebar: { open },
+        },
     },
-  },
-  ownProps
+    ownProps
 ) => {
-  const render =
-    ownProps.location.pathname !== "/signin" &&
-    ownProps.location.pathname !== "/signup";
+    const render =
+        ownProps.location.pathname !== '/signin' &&
+        ownProps.location.pathname !== '/signup'
 
-  const sidebarModalRender =
-    ownProps.location.pathname.startsWith("/videos") &&
-    !ownProps.location.pathname.endsWith("edit");
+    const sidebarModalRender =
+        ownProps.location.pathname.startsWith('/videos') &&
+        !ownProps.location.pathname.endsWith('edit')
 
-  return {
-    currentUser: users[session.id],
-    render: render,
-    open: open,
-    modal: modal,
-    sidebarModalRender,
-  };
-};
+    return {
+        currentUser: users[session.id],
+        render: render,
+        open: open,
+        modal: modal,
+        sidebarModalRender,
+    }
+}
 
 const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logout()),
-  openSidebar: () => dispatch(openSidebar()),
-  closeSidebar: () => dispatch(closeSidebar()),
-  openModal: (modal) => dispatch(openModal(modal)),
-});
+    logout: () => dispatch(logout()),
+    openSidebar: () => dispatch(openSidebar()),
+    closeSidebar: () => dispatch(closeSidebar()),
+    openModal: (modal) => dispatch(openModal(modal)),
+})
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
